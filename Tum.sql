754031835fd67907c566db09d119b7888cb3a10e
csoysal-odeal <cihan.soysal@ode.al>
2024-11-29 09:01:04 +0300

    uu

--- Tum.sql
+++ Tum.sql
@@ -257,12 +257,12 @@ select oo.id,me.identifier_no 'userId' , oo.unvan 'formattedName' , m.firstName 
        tx.code 'taxOfficeId',me.`type` , me.envelope_status, me.envelope_description
 from odeal.MerchantEnvelopeHistory me
 join odeal.Organisation oo on me.merchant_id  = oo.id
-join Merchant m on m.organisationId = oo.id and m.role = 0
+join odeal.Merchant m on m.organisationId = oo.id and m.role = 0
 left join odeal.TaxOffice tx on tx.id  = oo.taxOfficeId
 left join odeal.Town t on t.id = oo.townId
 left join odeal.City c on c.id = oo.cityId
 where 1=1
     and date(created_date) >= '2022-04-01'
-order by merchant_id
+order by merchant_id;
 
 SELECT * FROM odeal.EInvoice ei WHERE ei.created_date >= "2024-11-25 00:00:00" ORDER BY ei.created_date DESC LIMIT 10
